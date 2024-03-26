9ac1a895e5222ec0986b3e3064afb63dec9d305b
kah230895 <kaiqueoliveiraantonio@gmail.com>
2024-03-26 13:37:00 -0300

    Configurado o projeto para lidar com SVG

--- src/theme/light.ts
+++ src/theme/light.ts
@@ -6,6 +6,6 @@ export default {
     GRAY_100: "#f8f8f8",
     GRAY_200: "#e9e9e9",
     GRAY_300: "#212327",
-    GRAY_400: "rgba(186, 187, 188, 0.33)",
+    GRAY_400: "rgba(186, 187, 188, 0.70)",
   },
 } as DefaultTheme;
